9883f21764329f97fa129db7756d3386eade4a3e
KsanaSal <oasalivon@gmail.com>
2022-09-15 21:36:09 +0300

    Change in project

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const axios = require("axios").default;
+import axios from "axios";
 
 const apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
 let url = `https://api.openweathermap.org/data/2.5/weather`;
